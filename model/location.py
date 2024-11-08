eac74ac27363e172edcc41c2b0993019ef923915
Raul-Prisecaru <Raul_prisecaru123@protonmail.com>
2024-11-08 21:55:48 +0000

    Implemented Type Hints

--- model/location.py
+++ model/location.py
@@ -1,18 +1,18 @@
 class Location:
-    def __init__(self, x, y):
+    def __init__(self, x: int, y: int) -> None:
         self.__x = x
         self.__y = y
 
-    def get_x(self):
+    def get_x(self) -> int:
         return self.__x
     
-    def get_y(self):
+    def get_y(self) -> int:
         return self.get_y
     
-    def set_x(self, new_x):
+    def set_x(self, new_x: int) -> None:
         self.__x = new_x
 
-    def set_y(self, new_y):
+    def set_y(self, new_y: int) -> None:
         self.__y = new_y
     
         
