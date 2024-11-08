1a1427fe92b83941dd2b7a5eb8527e55c0a37d26
Raul-Prisecaru <Raul_prisecaru123@protonmail.com>
2024-11-08 22:04:23 +0000

    implemented getters and setters to retrieve the current location and set a new location

--- model/agent.py
+++ model/agent.py
@@ -4,4 +4,8 @@ class Agent:
     def __init__(self, location: Location):
         self.__location = location
 
+    def get_location(self):
+        return self.__location
     
+    def set_location(self, new_location: Location):
+        self.__location = new_location
