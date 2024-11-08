da8ad4333358f42faa36d52d3ce45e9e552b2277
Raul-Prisecaru <Raul_prisecaru123@protonmail.com>
2024-11-08 22:44:08 +0000

    implemented Act abstract and finished implementing Type Hints

--- model/agent.py
+++ model/agent.py
@@ -1,11 +1,16 @@
 from location import Location
+from abc import ABC, abstractmethod
 
 class Agent:
-    def __init__(self, location: Location):
+    def __init__(self, location: Location) -> None:
         self.__location = location
 
-    def get_location(self):
+    def get_location(self) -> Location:
         return self.__location
     
-    def set_location(self, new_location: Location):
+    def set_location(self, new_location: Location) -> None:
         self.__location = new_location
+
+    @abstractmethod
+    def act(self):
+        pass
