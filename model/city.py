14261ba23632ad82e8cc8dbadc6af42d884666d8
Raul-Prisecaru <Raul_prisecaru123@protonmail.com>
2024-11-09 14:09:57 +0000

    implemented to store location for free spots and any survivor_bots nearby

--- model/city.py
+++ model/city.py
@@ -4,6 +4,7 @@ from environment import Environment
 from location import Location
 from agent import Agent
 from typing import override, Optional, List
+from agents.survivor_bot import SurvivorBot
 
 
 class City(Environment, ABC):
@@ -27,5 +28,21 @@ class City(Environment, ABC):
 
 
     def find_free_spot(self, location: Location) -> List[Location]:
-        pass
+        normal_offsets = [
+            (-1, -1), (-1, 0), (-1, 1),
+            (0, -1),  (0, 0),  (0, 1),
+            (1, -1),  (1, 0),  (1, 1)
+        ]
+
+        for offset_x, offset_y in normal_offsets:
+
+            new_offset_x = location.get_x() + offset_x
+            new_offset_y = location.get_y() + offset_y
+
+            if self.__environment[new_offset_x][new_offset_y] is None:
+                self.__free_spots.append(Location(new_offset_x, new_offset_y))
+
+            if self.__environment[new_offset_x][new_offset_y] is SurvivorBot:
+                self.__robot_nearby.append(Location(new_offset_x, new_offset_y))
+
 
