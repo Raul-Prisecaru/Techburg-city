6fc899617ba3fa6e061966c4ff040e5b1945e5f3
Raul-Prisecaru <Raul_prisecaru123@protonmail.com>
2024-11-09 01:11:56 +0000

    City creates a grid based width and height provided

--- model/city.py
+++ model/city.py
@@ -1,7 +1,24 @@
+from abc import ABC
+
 from environment import Environment
-class City(Environment):
-    def __init__(self, width, height):
-        self.__width = width
-        self.__height = height
+from location import Location
+from agent import Agent
+from typing import override, Optional
+
+
+class City(Environment, ABC):
+    def __init__(self, width: int, height: int) -> None:
+        super().__init__(width, height)
+        self.__environment = []
+
+        for _ in range(height):
+            self.__environment.append([None] * width)
+
+    def get_agent(self, location: Location) -> Optional[Agent]:
+        pass
+    def set_agent(self, agent: Agent, location: Location) -> None:
+        pass
+
+
 
     
