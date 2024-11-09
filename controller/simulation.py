acbbf4d0646e24a7f7f1b620d9fc4e289adfbcf5
Raul-Prisecaru <Raul_prisecaru123@protonmail.com>
2024-11-09 17:35:15 +0000

    Implemented Comments to understand what code does when coming back at a later date

--- controller/simulation.py
+++ controller/simulation.py
@@ -9,13 +9,18 @@ class Simulation:
         self.__height = height
 
     def run(self):
+        # Creating and displaying City Environment
         city_environment = City(self.__width, self.__height)
         city_environment.display_environment()
+
+        # Creating SurvivorBot Agent and setting it to center location as start
         bot = SurvivorBot(Location(int(self.__width / 2), int(self.__height / 2)))
 
+        # Placing Agent in the Agents' set location
         city_environment.set_agent(bot, bot.get_location())
         city_environment.display_environment()
 
 
+# Running the Simulation
 Simulation(10, 10).run()
 
--- model/city.py
+++ model/city.py
@@ -5,6 +5,8 @@ from model.location import Location
 from model.agent import Agent
 from typing import override, Optional, List, TYPE_CHECKING
 
+# Importing these files if they are being used for Type_checking
+# Helps to avoid circular Imports
 if TYPE_CHECKING:
     from agents.survivor_bot import SurvivorBot
     from agents.malfunctioning_drone import MalfunctioningDrone
@@ -15,36 +17,52 @@ if TYPE_CHECKING:
 class City(Environment, ABC):
     def __init__(self, width: int, height: int) -> None:
         super().__init__(width, height)
+        # Environment where everything happens
         self.__environment = []
+        # Storing Free available spaces
         self.__free_spots = []
+        # Storing the coordinates of nearby Survivor Bot
         self.__survivor_bot_nearby = []
+        # Storing the coordinates of nearby Drones
         self.__malfunctioning_drone_nearby = []
+        # Storing the coordinates of nearby Scavenger Swarms
         self.__scavenger_swarm_nearby = []
+        # Storing the coordinates of nearby Spare Part
         self.__spare_part_nearby = []
+
         self.__height = height
+        self.__width = width
 
-        for _ in range(height):
-            self.__environment.append([None] * width)
+        # Creating Grid with for loop based on provided width and heights
+        for _ in range(self.__height):
+            self.__environment.append([None] * self.__width)
 
+    # Returns an Agent if found at provided location else None
     def get_agent(self, location: Location) -> Optional[Agent]:
         return self.__environment[location.get_x()][location.get_y()]
 
+    # Sets an Agent at provided location.
     def set_agent(self, agent: Agent, location: Location) -> None:
         self.__environment[location.get_x()][location.get_y()] = agent
 
-
+    # Function to Find free available spots around the location provided
     def find_free_spot(self, location: Location):
+        # "Coordinates" to indicate where to search for free spots
         normal_offsets = [
             (-1, -1), (-1, 0), (-1, 1),
             (0, -1),  (0, 0),  (0, 1),
             (1, -1),  (1, 0),  (1, 1)
         ]
 
+        # For Loop to check for Free Space
         for offset_x, offset_y in normal_offsets:
 
+            # Getting coordinates that reflect the position of the Agent
+            # E.g coordinates that are around the current Agent position
             new_offset_x = location.get_x() + offset_x
             new_offset_y = location.get_y() + offset_y
 
+            # Checking if there is something at those new coordinates
             if self.__environment[new_offset_x][new_offset_y] is None:
                 self.__free_spots.append(Location(new_offset_x, new_offset_y))
 
@@ -64,7 +82,7 @@ class City(Environment, ABC):
         # return self.__free_spots, self.__survivor_bot_nearby, self.__malfunctioning_drone_nearby, self.__scavenger_swarm_nearby, self.__spare_part_nearby
         return self.__free_spots
 
-
+    # Display Environment
     def display_environment(self):
         for row in range(self.__height):
             print(self.__environment[row])
