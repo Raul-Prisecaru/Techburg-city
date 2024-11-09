7bcc1fd70efa063772ace4a183645403fca5e9d7
Raul-Prisecaru <Raul_prisecaru123@protonmail.com>
2024-11-09 14:23:44 +0000

    Implemented to store location of Spare Parts

--- model/city.py
+++ model/city.py
@@ -7,6 +7,7 @@ from typing import override, Optional, List
 from agents.survivor_bot import SurvivorBot
 from agents.malfunctioning_drone import MalfunctioningDrone
 from agents.scavenger_swarm import ScavengerSwarm
+from agents.spare_part import SparePart
 
 
 class City(Environment, ABC):
@@ -53,6 +54,8 @@ class City(Environment, ABC):
             if self.__environment[new_offset_x][new_offset_y] is ScavengerSwarm:
                 self.__scavenger_swarm_nearby.append(Location(new_offset_x, new_offset_y))
 
+            if self.__environment[new_offset_x][new_offset_y] is SparePart:
+                self.__spare_part_nearby.append(Location(new_offset_x, new_offset_y))
 
 
 
