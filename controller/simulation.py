d831e8a8f445e0cafefb6c6562e85869b4e01be0
Raul-Prisecaru <Raul_prisecaru123@protonmail.com>
2024-11-09 17:58:41 +0000

    Bot can move with act function

--- controller/simulation.py
+++ controller/simulation.py
@@ -4,7 +4,7 @@ from model.agents.survivor_bot import SurvivorBot
 
 
 class Simulation:
-    def __init__(self, width, height):
+    def __init__(self, width: int, height: int) -> None:
         self.__width = width
         self.__height = height
 
@@ -12,12 +12,19 @@ class Simulation:
         # Creating and displaying City Environment
         city_environment = City(self.__width, self.__height)
         city_environment.display_environment()
+        print("----")
+
 
         # Creating SurvivorBot Agent and setting it to center location as start
         bot = SurvivorBot(Location(int(self.__width / 2), int(self.__height / 2)))
 
         # Placing Agent in the Agents' set location
-        city_environment.set_agent(bot, bot.get_location())
+        city_environment.set_agent("Bot", bot.get_location())
+        city_environment.display_environment()
+        print("----")
+        bot.act(city_environment)
+        print("----")
+
         city_environment.display_environment()
 
 
