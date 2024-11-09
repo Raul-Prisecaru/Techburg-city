2083fde1890ecc290bfe4a025d1819aa5a12e56e
Raul-Prisecaru <Raul_prisecaru123@protonmail.com>
2024-11-09 16:06:05 +0000

    Initiated class and created Act and __move functions

--- model/agents/survivor_bot.py
+++ model/agents/survivor_bot.py
@@ -1,2 +1,23 @@
-class SurvivorBot:
-    pass
+import os
+
+from abc import ABC
+from model.city import City
+from model.agent import Agent
+from model.location import Location
+current_dir = os.path.dirname(__file__)
+move_up_dir = os.path.dirname(current_dir)
+
+
+class SurvivorBot(Agent):
+    def __init__(self, location: Location):
+        super().__init__(location)
+
+
+    def act(self, city: City):
+        pass
+
+    def __move(self, city: City):
+        pass
+
+
+
