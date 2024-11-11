30f4510013ecae0fa0e1341ff687e2ddbcbf6fb1
Raul-Prisecaru <Raul_prisecaru123@protonmail.com>
2024-11-11 11:49:59 +0000

    Implemented: Added Testing for Location class for getters and setters

---
+++ test/test_location.py
@@ -0,0 +1,18 @@
+import unittest
+from model.location import Location
+
+class TestLocation(unittest.TestCase):
+
+    def test_getter(self):
+        location = Location(1, 2)
+        self.assertEqual(location.get_x(), 1, "X value was incorrectly returned")
+        self.assertEqual(location.get_y(), 2, "Y value was incorrectly returned")
+
+
+    def test_setter(self):
+        location = Location(1,2)
+        location.set_x(3)
+        location.set_y(4)
+
+        self.assertEqual(location.get_x(), 3, "X value was incorrectly updated")
+        self.assertEqual(location.get_y(), 4, "Y value was incorrectly updated")
