d91c32d423c191cfb3a2e970074875591b865f4c
Raul-Prisecaru <Raul_prisecaru123@protonmail.com>
2024-11-13 01:42:40 +0000

    Added: Added test to check if survivor bot moves towards the Spare Part in it's radius

--- test/test_robot.py
+++ test/test_robot.py
@@ -14,5 +14,19 @@ class TestRobotAgent(unittest.TestCase):
 
         self.assertNotEqual(Location(5, 5), survivor_bot.get_location(), "Survivor Bot did not move")
 
+    def test_pickUp_spare_part(self):
+        city = City(10, 10)
+        spare_part_place = Location(5, 6)
+        sparePart = SparePart(spare_part_place)
+
+        city.set_agent(sparePart, sparePart.get_location())
+        start_location = Location(5,5)
+        survivor_bot = SurvivorBot(start_location)
+        survivor_bot.act(city)
+
+        # Checking if bot went to the Items Location
+        self.assertEqual(survivor_bot.get_location(), spare_part_place, "Survivor bot did not go to the Spare Part Location to pick it up")
+
+
 
 
