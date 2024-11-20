3a77af9afe135f91c261d90f76a4927905e77d43
Raul-Prisecaru <Raul_prisecaru123@protonmail.com>
2024-11-20 00:04:32 +0000

    Changed: Removed Simulation code

--- controller/simulation.py
+++ controller/simulation.py
@@ -3,6 +3,7 @@ from model.location import  Location
 from model.agents.survivor_bot import SurvivorBot
 from model.agents.malfunctioning_drone import MalfunctioningDrone
 from model.agents.spare_part import SparePart
+from model.recharge_station import RechargeStation
 import time
 
 
@@ -11,57 +12,17 @@ class Simulation:
         self.__width = width
         self.__height = height
 
+
+
     def run(self):
+
         # Creating and displaying City Environment
         city_environment = City(self.__width, self.__height)
-        # city_environment.display_environment()
-        # print("----")
-
-        sparepart = SparePart(Location(5,5))
-
-        sparepart.act(city_environment, 1, 5, 5)
-        # city_environment.display_environment()
-
-
-
-        # Creating SurvivorBot Agent and setting a location as start
-        survivor_bot1 = SurvivorBot(Location(4, 5))
-        survivor_bot2 = SurvivorBot(Location(6, 6))
-        survivor_bot3 = SurvivorBot(Location(7, 7))
-        survivor_bot4 = SurvivorBot(Location(8, 8))
-
-        # Creating Malfunctioning Drone Agent and setting a location as start
-        malfunctioning_drone1 = MalfunctioningDrone(Location(1, 1))
-        malfunctioning_drone2 = MalfunctioningDrone(Location(2, 2))
-        malfunctioning_drone3 = MalfunctioningDrone(Location(3, 3))
-        malfunctioning_drone4 = MalfunctioningDrone(Location(4, 4))
-
-
-
-        # Placing Agent in the Agents' set location
-        city_environment.set_agent(survivor_bot1, survivor_bot1.get_location())
-        city_environment.set_agent(survivor_bot2, survivor_bot2.get_location())
-        city_environment.set_agent(survivor_bot3, survivor_bot3.get_location())
-        city_environment.set_agent(survivor_bot4, survivor_bot4.get_location())
-
-        city_environment.set_agent(malfunctioning_drone1, malfunctioning_drone1.get_location())
-        city_environment.set_agent(malfunctioning_drone2, malfunctioning_drone2.get_location())
-        city_environment.set_agent(malfunctioning_drone3, malfunctioning_drone3.get_location())
-        city_environment.set_agent(malfunctioning_drone4, malfunctioning_drone4.get_location())
 
         while True:
-            survivor_bot1.act(city_environment)
-            survivor_bot2.act(city_environment)
-            survivor_bot3.act(city_environment)
-            survivor_bot4.act(city_environment)
-
-            malfunctioning_drone1.act(city_environment)
-            malfunctioning_drone2.act(city_environment)
-            malfunctioning_drone3.act(city_environment)
-            malfunctioning_drone4.act(city_environment)
             print("---")
             city_environment.display_environment()
-            time.sleep(1)
+            time.sleep(5)
 
 
 
