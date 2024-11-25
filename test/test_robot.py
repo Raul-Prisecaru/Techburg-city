5e50079ba2bf19407366cb8e35d577e725b5bf69
Raul-Prisecaru <Raul_prisecaru123@protonmail.com>
2024-11-25 19:34:20 +0000

    Added: Recharge Stations to the existing Tests

--- test/test_robot.py
+++ test/test_robot.py
@@ -3,20 +3,32 @@ from model.agents.survivor_bot import SurvivorBot
 from model.location import Location
 from model.city import City
 from model.agents.spare_part import SparePart
+from model.recharge_station import RechargeStation
 
 
 class TestRobotAgent(unittest.TestCase):
+
     def test_movement(self):
+        self.__recharge_Station = RechargeStation()
+        self.__recharge_Station.set_location(Location(5, 9))
         city = City(10, 10)
         start_location = Location(5,5)
         survivor_bot = SurvivorBot(start_location)
+
+        survivor_bot.set_primary_recharge_station(self.__recharge_Station)
+
+
         survivor_bot.act(city)
 
         self.assertNotEqual(Location(5, 5), survivor_bot.get_location(), "Survivor Bot did not move")
 
     def test_pickUp_spare_part(self):
+        self.__recharge_Station = RechargeStation()
+        self.__recharge_Station.set_location(Location(5, 9))
+
         city = City(10, 10)
         survivor_bot = SurvivorBot(Location(5,5))
+        survivor_bot.set_primary_recharge_station(self.__recharge_Station)
 
         spare_part = SparePart(Location(5,4))
 
