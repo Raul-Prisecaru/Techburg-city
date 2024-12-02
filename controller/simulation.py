6df2f1000b7dde79d5abd56ba45f00a7aab31848
Raul-Prisecaru <Raul_prisecaru123@protonmail.com>
2024-12-02 19:27:05 +0000

    Changed: Began cleaning up simulation code by splitting into multiple functions

--- controller/simulation.py
+++ controller/simulation.py
@@ -12,14 +12,72 @@ class Simulation:
         self.__width = width
         self.__height = height
         self.__rechargeStation = RechargeStation()
-        self.__rechargeStation.set_location(Location(15,29))
         self.__sparePart = SparePart(Location(15, 27))
         self.__survivorBot = SurvivorBot(Location(15, 28))
 
+    def recharge_station_config(self):
+        """
+        Function responsible for configuring recharge Stations Settings
+            Parameter:
+                None
+
+            Return:
+                None
+        """
+        self.__rechargeStation.set_location(Location(15,29))
+        pass
+
+    def survivor_bots_config(self):
+        """
+        Function responsible for configuring Survivor Bots
+            Parameter:
+                None
+
+            Return:
+                None
+        """
+        self.__survivorBot.set_primary_recharge_station(self.__rechargeStation)
+
+
+    def malfunctioning_drones_config(self):
+        """
+        Function responsible for configuring Malfunctioning Drones
+            Parameter:
+                None
+
+            Return:
+                None
+        """
+        pass
+
+    def scavenger_swarms_config(self):
+        """
+        Function responsible for configuring Scavenger Swarms
+            Parameter:
+                None
+
+            Return:
+                None
+        """
+        pass
+
+    def spare_parts_config(self):
+        """
+        Function responsible for configuring Spare Parts
+            Parameter:
+                None
+
+            Return:
+                None
+        """
+        pass
+
     def run(self):
 
-        # Creating and displaying City Environment
+        # Creating
         city_environment = City(self.__width, self.__height)
+
+        # Adding To the environment
         city_environment.set_agent(self.__rechargeStation, self.__rechargeStation.get_location())
         city_environment.set_agent(self.__sparePart, self.__sparePart.get_location())
         city_environment.set_agent(self.__survivorBot, self.__survivorBot.get_location())
@@ -28,7 +86,7 @@ class Simulation:
         while True:
             print("---")
             city_environment.display_environment()
-            self.__survivorBot.act(city_environment, self.__rechargeStation)
+            self.__survivorBot.act(city_environment)
             time.sleep(2)
 
 
