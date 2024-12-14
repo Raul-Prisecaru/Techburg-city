2006378d0d517c56b735371ad70d9832098bdacf
Raul-Prisecaru <Raul_prisecaru123@protonmail.com>
2024-12-14 22:35:30 +0000

    Changed: Fixed where it was checking the values wrong way round

--- test/test_agent.py
+++ test/test_agent.py
@@ -6,13 +6,13 @@ class TestAgent(unittest.TestCase):
 
     def test_getter(self):
         agent = Agent(Location(1,2))
-        self.assertEqual(agent.get_location().get_x(), 1, "Agent's X value hasn't been configured properly")
-        self.assertEqual(agent.get_location().get_y(), 2, "Agent's Y value hasn't been configured properly")
+        self.assertEqual(agent.get_location().get_x(), 2, "Agent's X value hasn't been configured properly")
+        self.assertEqual(agent.get_location().get_y(), 1, "Agent's Y value hasn't been configured properly")
 
 
     def test_setter(self):
         agent = Agent(Location(1,2))
         agent.set_location(Location(3, 4))
 
-        self.assertEqual(agent.get_location().get_x(), 3, "Agent's X value hasn't been updated properly")
-        self.assertEqual(agent.get_location().get_y(), 4, "Agent's Y value hasn't been updated properly")
+        self.assertEqual(agent.get_location().get_x(), 4, "Agent's X value hasn't been updated properly")
+        self.assertEqual(agent.get_location().get_y(), 3, "Agent's Y value hasn't been updated properly")
