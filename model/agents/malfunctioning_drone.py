570747bace8cea63127f5510921897c3d900638e
Raul-Prisecaru <Raul_prisecaru123@protonmail.com>
2024-12-15 19:25:08 +0000

    Added: Type Hints

--- model/agents/malfunctioning_drone.py
+++ model/agents/malfunctioning_drone.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 import os
 from abc import ABC
 import random
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, List
 
 from model.agent import Agent
 from model.location import Location
@@ -19,35 +19,35 @@ class MalfunctioningDrone(Agent):
     def __init__(self, location: Location):
         super().__init__(location)
 
-        self.__survivor_bot_list = []
+        self.__survivor_bot_list: List[Location] = []
 
 
 
-    def act(self, city: City):
+    def act(self, city: City) -> None:
 
         self.__survivor_bot_list = city.find_survivor_bot(self.get_location())
 
         if len(self.__survivor_bot_list) > 0:
-            self.__move_towards_survivor_bot()
+            self.__move_towards_survivor_bot(city)
 
         self.__move(city)
 
-    def __move(self, city: City):
-        current_location = self.get_location()
-        next_position_list = city.find_free_spot(current_location)
-        next_position = random.choice(next_position_list)
+    def __move(self, city: City) -> None:
+        previous_location: Location = self.get_location()
+        next_position_list = city.find_free_spot(previous_location)
+        next_position: Location = random.choice(next_position_list)
 
         city.set_agent(self, next_position)
 
         self.set_location(next_position)
 
-        city.set_agent(None, current_location)
+        city.set_agent(None, previous_location)
 
 
-    def __move_towards_survivor_bot(self, city: City):
-        current_location = self.get_location()
+    def __move_towards_survivor_bot(self, city: City) -> None:
+        current_location: Location = self.get_location()
 
-        next_position = random.choice(self.__survivor_bot_list)
+        next_position: Location = random.choice(self.__survivor_bot_list)
 
         city.set_agent(self, next_position)
 
