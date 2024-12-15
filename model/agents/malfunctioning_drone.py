bb34ee22120d1d3141886374af7bd91de8fb7cf4
Raul-Prisecaru <Raul_prisecaru123@protonmail.com>
2024-12-15 19:22:10 +0000

    Implemented: Function to move closer to Survivor Bot

--- model/agents/malfunctioning_drone.py
+++ model/agents/malfunctioning_drone.py
@@ -19,8 +19,17 @@ class MalfunctioningDrone(Agent):
     def __init__(self, location: Location):
         super().__init__(location)
 
+        self.__survivor_bot_list = []
+
+
 
     def act(self, city: City):
+
+        self.__survivor_bot_list = city.find_survivor_bot(self.get_location())
+
+        if len(self.__survivor_bot_list) > 0:
+            self.__move_towards_survivor_bot()
+
         self.__move(city)
 
     def __move(self, city: City):
@@ -33,6 +42,20 @@ class MalfunctioningDrone(Agent):
         self.set_location(next_position)
 
         city.set_agent(None, current_location)
+
+
+    def __move_towards_survivor_bot(self, city: City):
+        current_location = self.get_location()
+
+        next_position = random.choice(self.__survivor_bot_list)
+
+        city.set_agent(self, next_position)
+
+        self.set_location(next_position)
+
+        city.set_agent(None, current_location)
+
+
 
 
 
