43f91d11b3af3d406e3bd6e0251dbd372fffa249
Raul-Prisecaru <Raul_prisecaru123@protonmail.com>
2025-01-14 18:33:13 +0000

    Feat: Added MALFUNCTIONING_DRONE_MAX_ENERGY

--- controller/Config.py
+++ controller/Config.py
@@ -1,6 +1,7 @@
 class Config:
     """Class representing configuration parameters for a simulation."""
 
+    # GUI Settings
     SIM_NAME = "Ocean Simulation"
     OCEAN_SIZE = 20
 
@@ -9,3 +10,11 @@ class Config:
     SCAVENGER_SWARM_COLOUR = "green"
     RECHARGE_STATION_COLOUR = "purple"
     SPARE_PART_COLOUR = "black"
+
+
+    # Malfunctioning Drone Settings
+    MALFUNCTIONING_DRONE_MAX_ENERGY = 100
+
+
+
+
