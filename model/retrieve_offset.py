807e0b65b55857eb4c2d01fa59a76c132a5cf3ca
Raul-Prisecaru <Raul_prisecaru123@protonmail.com>
2025-01-14 05:05:32 +0000

    Feat: Added simple rule-based class for retrieving offsets

---
+++ model/retrieve_offset.py
@@ -0,0 +1,19 @@
+from typing import Tuple, List
+
+
+class RetrieveOffset:
+    def __init__(self):
+        self.__rules = []
+
+
+
+    def __add_rule(self, condition: int, result: List[Tuple[int, int]]):
+        self.__rules.append((condition, result))
+
+
+
+    def retrieve_offset(self, percentageValue: int):
+        for condition, result in self.__rules:
+            if condition(percentageValue):
+                return result
+
