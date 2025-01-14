5a9c9fb1631035ce71008f5fb3b2207bd1c61486
Raul-Prisecaru <Raul_prisecaru123@protonmail.com>
2025-01-14 05:37:00 +0000

    Feat: Expanded add_rule functions to include one for each enhancement and converted self.__rules from tuple to dict

--- model/retrieve_offset.py
+++ model/retrieve_offset.py
@@ -3,14 +3,29 @@ from typing import Tuple, List
 
 class RetrieveOffset:
     def __init__(self):
-        self.__rules = []
+        self.__rules = {
+
+        }
 
 
 
-    def add_rule(self, condition: int, result: List[Tuple[int, int]]):
-        self.__rules.append((condition, result))
+    def add_rule_for_speed_enhancement(self, condition: int, result: List[Tuple[int, int]]):
+        if "speed" is not self.__rules:
+            self.__rules["speed"] = []
 
+        self.__rules["speed"].append((condition, result))
 
+    def add_rule_for_vision_enhancement(self, condition: int, result: List[Tuple[int, int]]):
+        if "vision" is not self.__rules:
+            self.__rules["vision"] = []
+
+        self.__rules["vision"].append((condition, result))
+
+    def add_rule_for_energy_enhancement(self, condition: int, result: List[Tuple[int, int]]):
+        if "energy" is not self.__rules:
+            self.__rules["energy"] = []
+
+        self.__rules["energy"].append((condition, result))
 
     def retrieve_offset(self, percentageValue: int):
         for condition, result in self.__rules:
