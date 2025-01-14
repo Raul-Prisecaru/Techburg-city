28ef52ec261995d7aef273f60974c8a2fc3c4a10
Raul-Prisecaru <Raul_prisecaru123@protonmail.com>
2025-01-14 21:08:51 +0000

    Feat: added getters and setters for size and enhancement

--- model/agents/spare_part.py
+++ model/agents/spare_part.py
@@ -1,27 +1,41 @@
 from __future__ import annotations
 from random import random, randint
-from typing import TYPE_CHECKING, List
-
-from model.agent import Agent
-from model.location import Location
+from typing import TYPE_CHECKING, List, Literal
 
 if TYPE_CHECKING:
     from model.city import City
+    from model.location import Location
+
 
 class SparePart:
 
     def __init__(self, city: City):
-        self.__city = city
-        self.__location = None
+        self.__city: City = city
+        self.__location: Location = None
+        self.__size: Literal["Small", "Medium", "Large"] = None
+        self.__enhancement: Literal["Speed", "Vision", "Energy"] = None
 
     def get_location(self):
         return self.__location
 
+
+    def get_size(self):
+        return self.__size
+
+
+    def set_size(self, newSize: Literal["Small", "Medium", "Large"]):
+        self.__size = newSize
+
+    def get_enhancement(self):
+        return self.__enhancement
+
+    def set_enhancement(self, newEnhancement: Literal["Speed", "Vision", "Energy"]):
+        self.__enhancement = newEnhancement
+
+
     def add_specific_spot(self, location: Location):
         self.__location = location
         self.__city.add_object(location, self)
-        # self.__city.add_objects_to_map(location, self)
-
 
 
     def randomly_scatter(self, number_spare_parts: int, start_location: int, end_location: int) -> None:
